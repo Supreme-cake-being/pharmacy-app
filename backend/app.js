fbaf04495851b03e9b4181690e46daae06a1fba2
Karr4 <102288606+Karr4@users.noreply.github.com>
2024-03-02 21:30:46 +0200

    added server start

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,28 @@
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 
 const app = express();
 
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+
 app.use(cors());
 app.use(express.json());
-
-// { typeDefs, resolvers }
-
-const server = new ApolloServer();
-
 app.use('/graphql', expressMiddleware(server));
 
 export default app;
